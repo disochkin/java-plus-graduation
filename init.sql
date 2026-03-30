c421223699127be52b2485652f65f52829bef6b1
Denis Nagovitsyn <disochkin@gmail.com>
2026-03-30 18:14:53 +0000

    первая попытка

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE main_service_db;
+CREATE DATABASE stat_service_db;
+
+GRANT ALL PRIVILEGES ON DATABASE main_service_db TO ewmuser;
+GRANT ALL PRIVILEGES ON DATABASE stat_service_db TO ewmuser;
+
+
+
