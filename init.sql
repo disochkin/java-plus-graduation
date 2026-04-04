b58eb37b9c29a4cab6707e9550fe806d807ef8ea
Denis Nagovitsyn <disochkin@gmail.com>
2026-04-04 07:03:48 +0000

    добавил заготовку для request service

--- init.sql
+++ init.sql
@@ -1,12 +1,15 @@
 CREATE DATABASE main_service_db;
 CREATE DATABASE stat_service_db;
 CREATE DATABASE user_service_db;
-pfuCREATE DATABASE event_service_db;
+CREATE DATABASE event_service_db;
+CREATE DATABASE request_service_db;
 
 GRANT ALL PRIVILEGES ON DATABASE main_service_db TO ewmuser;
 GRANT ALL PRIVILEGES ON DATABASE stat_service_db TO ewmuser;
 GRANT ALL PRIVILEGES ON DATABASE user_service_db TO ewmuser;
 GRANT ALL PRIVILEGES ON DATABASE event_service_db TO ewmuser;
+GRANT ALL PRIVILEGES ON DATABASE request_service_db TO ewmuser;
+
 
 
 
