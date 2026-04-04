7d809700602807f33c802b185a1e806ba4b4e970
Denis Nagovitsyn <disochkin@gmail.com>
2026-04-04 06:46:55 +0000

    добавил заготовку для event-service

--- init.sql
+++ init.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE main_service_db;
 CREATE DATABASE stat_service_db;
 CREATE DATABASE user_service_db;
+pfuCREATE DATABASE event_service_db;
 
 GRANT ALL PRIVILEGES ON DATABASE main_service_db TO ewmuser;
 GRANT ALL PRIVILEGES ON DATABASE stat_service_db TO ewmuser;
 GRANT ALL PRIVILEGES ON DATABASE user_service_db TO ewmuser;
+GRANT ALL PRIVILEGES ON DATABASE event_service_db TO ewmuser;
 
 
 
