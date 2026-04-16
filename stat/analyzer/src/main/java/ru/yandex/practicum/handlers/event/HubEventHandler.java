aa4d4d18994e45122e86308aae50cc7b293b42e8
Denis Nagovitsyn <disochkin@gmail.com>
2026-04-16 16:04:16 +0000

    add analyzer etc

--- stat/analyzer/src/main/java/ru/yandex/practicum/handlers/event/HubEventHandler.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.handlers.event;
-
-import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
-
-public interface HubEventHandler {
-    void handle(HubEventAvro event);
-
-    String getPayloadType();
-}
